a6980f9a3d36ff930ce6882b425c23f897d9cfe6
yusuke-fujita <yusuke.fujita@dena.com>
2014-03-05 09:18:02 +0900

    設定画面のスピナー追加

--- gen/own/example/rakuraku/R.java
+++ gen/own/example/rakuraku/R.java
@@ -16,6 +16,10 @@ public final class R {
         public static final int abc_slide_out_bottom=0x7f040004;
         public static final int abc_slide_out_top=0x7f040005;
     }
+    public static final class array {
+        public static final int spinAge=0x7f0c0001;
+        public static final int spinSex=0x7f0c0000;
+    }
     public static final class attr {
         /**  Custom divider drawable to use for elements in the action bar. 
          <p>Must be a reference to another resource, in the form "<code>@[+][<i>package</i>:]<i>type</i>:<i>name</i></code>"
@@ -1110,12 +1114,11 @@ containing a value of this type.
         public static final int activity_item=0x7f030017;
         public static final int activity_main=0x7f030018;
         public static final int activity_popular=0x7f030019;
-        public static final int activity_setting=0x7f03001a;
-        public static final int ranking_list_item=0x7f03001b;
-        public static final int support_simple_spinner_dropdown_item=0x7f03001c;
+        public static final int ranking_list_item=0x7f03001a;
+        public static final int support_simple_spinner_dropdown_item=0x7f03001b;
     }
     public static final class menu {
-        public static final int main=0x7f0c0000;
+        public static final int main=0x7f0d0000;
     }
     public static final class string {
         /**  Content description for the action bar "home" affordance. [CHAR LIMIT=NONE] 
